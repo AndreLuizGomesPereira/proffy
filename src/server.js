ebde178073c774eb1fbc44bd0abe3b0f505b223f
André Luiz <andregomesenator@gmail.com>
2020-08-08 17:37:19 -0300

    Finalizado a rotina de cadastro de proffys

--- src/server.js
+++ src/server.js
@@ -12,17 +12,59 @@ const proffys = [
     }
 ]
 
+const subjects = [
+    "Artes",
+    "Biologia",
+    "Ciências",
+    "Desenvolvimento WEB",
+    "Educação Física",
+    "Filosofia",
+    "Física",
+    "Geografia",
+    "História",
+    "Inglês",
+    "Matemática",
+    "Portugês",
+    "Sociologia",
+    "Redação",
+    "Química"
+]
+
+const weekdays = [
+    "Domingo",
+    "Segunda-feira",
+    "Terça-feira",
+    "Quarta-feira",
+    "Quinta-feira",
+    "Sexta-feira",
+    "Sábado"
+]
+
+function getSubject(subjectNumber) {
+    const position = +subjectNumber - 1
+    return subjects[position]
+}
+
 
 function pageLanding(req, res) {
     return res.render("index.html")
 }
 // Parado o video 1:15.
 function pageStudy(req, res) {
-    return res.render("study.html", { proffys })
+    const filters = req.query
+    return res.render("study.html", { proffys, filters, subjects, weekdays })
 }
 
 function pageGiveClasses(req, res) {
-    return res.render("give-classes.html")
+    const data = req.query
+    const isNotEmpty = Object.keys(data).length > 0
+    if (isNotEmpty) {
+        data.subject = getSubject(data.subject)
+        proffys.push(data)
+        return res.redirect("/study")
+    }
+
+    return res.render("give-classes.html", { subjects, weekdays })
 }
 
 const express = require('express')
